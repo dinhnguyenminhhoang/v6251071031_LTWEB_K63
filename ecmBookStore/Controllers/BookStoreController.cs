1c33817cda9a393318c22d1700e36e2b3b0b24fb
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2024-11-01 16:41:13 +0700

    [feat]:lab-1-01/11/2024

---
+++ ecmBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,62 @@
+﻿using ecmBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ecmBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        private ecomBookStoreEntities db = new ecomBookStoreEntities();
+        public ActionResult Index()
+        {
+            var books = db.SACH.ToList();
+            ViewBag.Books = books;
+            return View();
+        }
+        public ActionResult ChuDe()
+        {
+            var chuDe = db.CHUDE.ToList();
+            return PartialView(chuDe);
+        }
+        public ActionResult NhaXuatBan()
+        {
+            var nhaXuatBan = db.NHAXUATBAN.ToList();
+            return PartialView(nhaXuatBan); 
+        }
+        public ActionResult Details(string id)
+        {
+            var book = db.SACH.FirstOrDefault(b => b.MaSach == id); 
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book); 
+        }
+        public ActionResult SpTheoNSX(string id)
+        {
+            var books = db.SACH.Where(b => b.MaNXB == id).ToList();
+
+            if (books == null || !books.Any())
+            {
+                return HttpNotFound(); 
+            }
+            ViewBag.Books = books;
+            return View(); 
+        }
+        public ActionResult SpTheoChuDe(string id)
+        {
+            var books = db.SACH.Where(b => b.MaCD == id).ToList();
+
+            if (books == null || !books.Any())
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Books = books;
+            return View();
+        }
+    }
+}
---
+++ ecmBookStore/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ecmBookStore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Masach { get; set; }
+        public string Tensach { get; set; }
+        public decimal Giaban { get; set; }
+        public string Mota { get; set; }
+        public string Anhbia { get; set; }
+        public DateTime? Ngaycapnhat { get; set; }
+        public int Soluongton { get; set; }
+        public int MaCD { get; set; }
+        public int MaNXB { get; set; }
+    }
+}
